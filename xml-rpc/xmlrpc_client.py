2af36d77e6f83537216b99208f5be0bbc5a010ea
korobool <korobov.alex@gmail.com>
2012-11-11 14:02:19 +0200

    XML-RPC service contact updated.

--- xml-rpc/xmlrpc_client.py
+++ xml-rpc/xmlrpc_client.py
@@ -1,11 +1,6 @@
-__author__ = 'Alien'
+__author__ = 'Oleksandr Korobov'
 from xmlrpclib import ServerProxy
-connect = ServerProxy("http://localhost:8000")
-#s = ServerProxy("http://user:pass@host:8080")
+connect = ServerProxy("http://korobov-labs.com:8000")
 print connect.system.listMethods()
-key = connect.login("root", "xn3ma8")
-print connect.add_num(key, 5, 10)
-print connect.add_str(key, 5, 10)
-print connect.repeater(key, "Hello!")
-print connect.repeater(key, 500)
-raw_input()
+key = connect.login('She was in love but he has killed her. After this murder he was arrested.')
+print key
--- xml-rpc/xmlrpc_server.py
+++ xml-rpc/xmlrpc_server.py
@@ -1,48 +1,13 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 import random
+import cls
 
 class ServerClass():
     def __init__(self):
-        self.authenticated = []
-        self.username = "root"
-        self.password = "xn3ma8"
-
-    def hello_hello(self):
-        pass
-    
-    def login(self, username, password):
-        key = 0.0
-        if (self.username == username) and (self.password == password):
-            key = random.uniform(1, 100)
-            self.authenticated.append(key)
-            print key
-        return key
-
-    def __check___(self, key):
-        flag = False
-        #print self.authenticated
-        for one in self.authenticated:
-            #print one
-            if one == key:
-                flag = True
-        return flag
-
-    def add_num(self, key, a, b):
-        if self.__check___(key):
-            #print key
-            return a + b
-
-    def add_str(self, key, a, b):
-        if self.__check___(key):
-            #print key
-            return str(a) + str(b)
-
-    def repeater(self, key, something):
-        if self.__check___(key):
-            if isinstance(something, str):
-                return "You passed the string: " + something
-            else:
-                return "You have to pass a string!"
+        self.ai_agent = cls.Classifier()
+    def classify_text(self, text):
+        genre = self.ai_agent.classify(text)
+        return genre
 
 server = SimpleXMLRPCServer(("", 8000))
 server.register_instance(ServerClass())
