9ec521107f3b902a1e5b1909369fd32c513cf238
korobool <korobov.alex@gmail.com>
2012-11-20 16:33:51 +0200

    Saving to file added.

--- classifier/classifier/view.py
+++ classifier/classifier/view.py
@@ -1,25 +1,35 @@
-
+import os
+import datetime
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
 from model import text_processor, get_known_genres_list
 import smtplib
 
-def mail(msg):
+#def mail(msg):
+#    try:
+#        fromaddr = 'oleksandr.korobov@gmail.com'
+#        toaddrs  = 'oleksandr.korobov@gmail.com'
+#        # msg = 'Message'
+#
+#        # Credentials (if needed)
+#        username = 'oleksandr.korobov@gmail.com'
+#        password = 'grammarly.c0m'
+#
+#        # The actual mail send
+#        server = smtplib.SMTP('smtp.gmail.com:587')
+#        server.starttls()
+#        server.login(username,password)
+#        server.sendmail(fromaddr, toaddrs, msg)
+#        server.quit()
+#    except:
+#        pass
+
+def save(msg):
     try:
-        fromaddr = 'oleksandr.korobov@gmail.com'
-        toaddrs  = 'oleksandr.korobov@gmail.com'
-        # msg = 'Message'
-
-        # Credentials (if needed)
-        username = 'oleksandr.korobov@gmail.com'
-        password = 'grammarly.c0m'
-
-        # The actual mail send
-        server = smtplib.SMTP('smtp.gmail.com:587')
-        server.starttls()
-        server.login(username,password)
-        server.sendmail(fromaddr, toaddrs, msg)
-        server.quit()
+        time = datetime.datetime.now()
+        file = open('/home/ubuntu/texts/' + str(time) + '.txt', 'rw')
+        file.write(msg)
+        file.close()
     except:
         pass
 
@@ -38,7 +48,7 @@ def processing(request):
         if request.POST['processing-text'] != u'':
             text = request.POST['processing-text']
             genre = text_processor(text)
-            mail('________________________\n' + genre + '________________________\n\n\n\n' + text)
+            save('________________________\n' + genre + '________________________\n\n\n\n' + text)
         else:
             genre = "Empty textarea!"
     else:
